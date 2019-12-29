d5efff478ad833ec5c36cdefd75d5a7011f55b00
KseniaPenyevskaya <56612695+KseniaPenyevskaya@users.noreply.github.com>
2019-12-29 16:03:04 +0300

    Add files via upload

--- TravellingSalesmanProblem/mst.cpp
+++ TravellingSalesmanProblem/mst.cpp
@@ -56,9 +56,7 @@ double apprMinWeight(Graph& gr) {
 
 	Graph mst = getMinimumSpanningTree(gr);
 	mystack.push(0);
-	int si = mystack.size();
 
-	std::cout << si << std::endl;
 	while (!mystack.empty()) {
 		vert = mystack.top();
 		mystack.pop();
@@ -77,3 +75,48 @@ double apprMinWeight(Graph& gr) {
 	}
 	return weight;
 }
+
+//double apprWeight() {
+//	vector <bool> visited(gr.GetVerticesCount(), false);
+//}
+
+/*
+double getMinimumSpanningTreeWeight(Graph& gr) {
+
+	int verticesCount = gr.GetVerticesCount();
+
+	vector <double> minWeightFromVert(verticesCount, DBL_MAX);
+	vector <bool> addedVertState(verticesCount, false);
+	vector <int> minSpTreeVert;
+
+	set <pair<double, int> > vertSet;
+	vertSet.insert(make_pair(0, 0));
+	minWeightFromVert[0] = 0.0;
+
+	double totalWeight = 0.0;
+	double weight;
+	int vert;
+	double nWeight;
+	int nVert;
+	while (minSpTreeVert.size() < verticesCount) {
+		weight = vertSet.begin()->first;
+		vert = vertSet.begin()->second;
+		totalWeight += weight;
+		minSpTreeVert.push_back(vert);
+		addedVertState[vert] = true;
+		vertSet.erase(vertSet.begin());
+		for (GraphsEdge nextVert : gr.GetNextVertices(vert)) {
+			nVert = nextVert.vert;
+			if (!addedVertState[nVert]) {
+				nWeight = nextVert.weight;
+				if (nWeight < minWeightFromVert[nVert]) {
+					vertSet.erase(make_pair(minWeightFromVert[nVert], nVert));
+					minWeightFromVert[nVert] = nWeight;
+					vertSet.insert(make_pair(nWeight, nVert));
+				}
+			}
+		}
+	}
+	return totalWeight;
+}
+*/
